c17bdb9488e4cfe2a2b3bfd254a5a8b0b06ed02c
bhavya405 <143187883+bhavya405@users.noreply.github.com>
2025-04-10 15:07:52 -0700

    finalll

--- ircc-storyboard/src/app/layout.tsx
+++ ircc-storyboard/src/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Providers>
           <ThemeProvider>
-            <ThemeToggle />
+         
             {children}
           </ThemeProvider>
         </Providers>
