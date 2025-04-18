41082c7131a7f33dbdd3e8dd05f82fba5568fe99
Saatvikbirla <97305571+Saatvikbirla@users.noreply.github.com>
2025-04-18 15:28:32 -0700

    header bar

--- ircc-storyboard/src/app/about/page.tsx
+++ ircc-storyboard/src/app/about/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Header from '@/components/Header'
 import { useRouter } from 'next/navigation'
 import { Footer } from '@/components/Footer'
 import {
@@ -41,6 +42,7 @@ export default function AboutPage() {
 
   return (
     <div className="min-h-screen flex flex-col">
+      <Header />
       <main className="flex-grow">
         <section className="w-full bg-gradient-to-br from-blue-50 to-white py-16 md:py-24">
           <div className="container mx-auto px-6">
--- ircc-storyboard/src/app/knowtheteam/page.tsx
+++ ircc-storyboard/src/app/knowtheteam/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Header from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import { LinkedinIcon, TwitterIcon } from 'lucide-react'
 
@@ -50,6 +51,7 @@ export default function knowtheteamPage() {
 
   return (
     <div className="min-h-screen flex flex-col">
+      <Header />
       <main className="flex-grow">
         <section className="w-full bg-gradient-to-br from-blue-50 to-white py-16 md:py-24">
           <div className="container mx-auto px-6">
